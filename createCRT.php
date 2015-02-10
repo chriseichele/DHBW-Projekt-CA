9c7d051cb18ade69b14951855e3c7abf3af31a0d
NotBuildingSoftware <catch-a-ride@web.de>
2015-02-10 15:30:33 +0100

    Update createCRT.php

--- createCRT.php
+++ createCRT.php
@@ -4,33 +4,11 @@
 	#output .crt Datei:  
 		function createCertificate($dir){
 			#test-csr
-			#echo $dir."<br>";
-			global $exp;
-			$csr = "/var/www/html/".$exp."/retina.csr";
-			#echo "$csr";	
-			#echo("<br>");
-			shell_exec("cd /var/www/html/");
-			shell_exec("mkdir lol");
-			#Ordner für die crt Datei anlegen
-			$cmd = "/var/www/html/".$dir."/";
-			#echo($cmd."<br>");
-			#shell_exec("mkdir "+$cmd);
-			
-			#Config-Datei auswählen	
-			#$conf = "/home/arne/ssl/ca.cnf";	
-			
-			#Zertifikat erstellen	
-			$crtcmd = "openssl ca -config /home/arne/ssl/certificat.cnf -in ".$csr.".csr -out ".$cmd."example.crt";
-			#echo"$crtcmd";
-			$test = shell_exec($crtcmd);
-			#echo "$test";
-			#Ergebnis in Datei schreiben
-		
+		$res = shell_exec("sudo /bin/createCRT.sh ".$dir);
+		echo($res);
 		}
 		
 		$exp = $_GET["test"];
-		#echo($exp);
-		#echo('<br>');
 		createCertificate($exp);
 	
 	#openssl ca -config /home/arne/ssl/certificat.cnf -in /home/arne/ssl/example.com/example.csr -out /home/arne/ssl/example.crt
