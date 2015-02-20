ec26129246b8ee8a0060ce5c946570c07f76bc62
chriseichele <youshallnotspam+github@gmail.com>
2015-02-20 12:22:32 +0100

    Weiterentwicklung CSR Download Seite

--- mycsr.php
+++ mycsr.php
@@ -8,7 +8,14 @@ doUserRightsCheck();
 $pagetitle = "Meine Zertifikatsanfragen";
 
 include('./header.inc');
-
+?>
+<div class="jumbotron">
+	<div class="container">
+		<h1>&Uuml;bersicht meiner eingereichten Zertrifikats&shy;anfragen mit Status</h1>
+		<p>Mit dem dazugeh&ouml;rigen Link können Sie Ihre Anfragen zu &Uuml;berpr&uuml;fung downloaden.</p>     
+	</div>
+</div>
+<?php
 echo '<div class="container">';
 echo CrtDownloader::getUserRequestList();
 echo '</div>';
