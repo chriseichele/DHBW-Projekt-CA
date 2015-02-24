2da068cf47160ac4ca2d038b88dc68cdeeeb612d
chriseichele <youshallnotspam+github@gmail.com>
2015-02-24 22:39:40 +0100

    Button Design

--- zertifikatanfragen.php
+++ zertifikatanfragen.php
@@ -13,7 +13,9 @@ if(isset($_GET['csr'])) {
 }
 $db = new DBAccess();
 $where = array("id","=","'".$csr_id."'");
-$csr = get_object_vars(reset($db->get_request_all_where($where)));
+$dbresult = $db->get_request_all_where($where);
+$csrs = reset($dbresult);
+$csr = get_object_vars($csrs);
 
 if($csr['status'] != 'created') {
   	$_SESSION['message']['warning'][] = "CSR wurde bereits bearbeitet!";
@@ -40,11 +42,11 @@ include('./header.inc');
 	</table>
     <form method="post" action="validateCSR.php" class="form-inline">
     	<input type="hidden" name="csr" value="<?php echo $csr_id; ?>">
-    	<div class="form-group">
-    		<button type="submit" name="accept" value="true" class="btn btn-primary btn-lg" role="button">Anfrage annehmen</a>
+    	<div class="form-group col-md-6">
+    		<button type="submit" name="accept" value="true" class="btn btn-success btn-lg btn-block" role="button">Anfrage annehmen</a>
     	</div>
-    	<div class="form-group">
-    		<button type="submit" name="accept" value="false" class="btn btn-danger btn-lg" role="button">Anfrage ablehnen</a>
+    	<div class="form-group col-md-6">
+    		<button type="submit" name="accept" value="false" class="btn btn-danger btn-lg btn-block" role="button">Anfrage ablehnen</a>
     	</div>
     </form>
 </div>
