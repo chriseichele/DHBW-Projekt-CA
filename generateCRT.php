395ba181e4516ef0715c44abf73fc126bd4722cc
NotBuildingSoftware <catch-a-ride@web.de>
2015-02-26 18:37:55 +0100

    Update generateCRT.php
    openssl angepasst

--- generateCRT.php
+++ generateCRT.php
@@ -5,20 +5,21 @@ require_once('./db.php');
 		function createCertificate($id){
 			#TODO: Abfragen ob der User eingeloggt  ist
 			
-			connect();
-			$pathToCSR = select("path", "request", "id = ".$id);
+			$db = new DBAccess();
+			$pathToCSR = $db->select("path", "request", "id = ".$id);
+			$name = $db->select("common_name","request", "id = ".$id);
 			#Prüfung ob die Select-Abfrage erfolgreich war
 			if($pathToCSR == NULL)
 				{
 					throw new Exception("Etwas blödes ist passiert. Das tut uns leid. Fehler 1");
 				}
 			elseif {
-				$update = update_request_status("id = ".$id, 3);
+				$update = $db->update_request_status("id = ".$id, 3);
 				#Prüfung ob die Update-Abfrage erfolgreich war			
 				if(isset($update)){
 					#TODO: Pfad muss im Shell Skript angepasst werden
 					#TODO: Pfad muss angepasst werden an den Ort des Skriptes auf dem Server angepasst werden.
-					shell_exec("sudo /bin/createCRT.sh ".$pathToCSR);
+					shell_exec("c:\apache24\bin\openssl.exe ca -config c:\apache24\htdocs\dev\arne\certificat.cnf -in ".$pathToCSR." -out c:\apache24\htdocs\".$name." -batch);
 					return true;
 				}
 				else{
