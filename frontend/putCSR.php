7b050de9372c7c5776ce453ecac510676d37354b
NotBuildingSoftware <catch-a-ride@web.de>
2015-03-02 17:37:07 +0100

    Update putCSR.php

--- frontend/putCSR.php
+++ frontend/putCSR.php
@@ -1,57 +1,111 @@
 <?php
-require_once('./db.php');	
-#input id: id für das Zertifikat in der DB
-#output .crt Datei:  
-		function createCertificate($id){
-			#TODO: Abfragen ob der User eingeloggt  ist
-			
-			$db = new DBAccess();
-			$where = array("id","=","'".$id."'");
-			$db_result = $db->get_request_all_where($where);
-			$csr = reset($db_result);
-			$pathToCSR = $csr->path_csr;
-			$name = $csr->common_name;
-			$start = $csr->start;
-			$end = $csr->end;
-			$duration = 365 * ($end - $start);
-			#Prüfung ob die Select-Abfrage erfolgreich war
-			if($pathToCSR == NULL) {
-				throw new Exception("Der Pfad zur CSR Datei konnte nicht ermittelt werden!");
-			}
-			else {
-				$update = $db->update_request_status($where, 3);
-				#Prüfung ob die Update-Abfrage erfolgreich war			
-				if(isset($update['affected_rows'])){
-					#TODO: Pfad muss im Shell Skript angepasst werden
-					#TODO: Pfad muss angepasst werden an den Ort des Skriptes auf dem Server angepasst werden.
-					$pathToCRT = trim("c:\apache24\ca\kunden\crt\\".$name).".crt";
-					#shell_exec("c:\apache24\bin\openssl.exe ca -config c:\apache24\htdocs\dev\arne\certificate.cnf -in ".$pathToCSR." -out ".$pathToCRT." -batch");
-					shell_exec("c:\apache24\bin\openssl.exe x509 -req -config c:\apache24\htdocs\dev\arne\intermediate.cnf -in ".$pathToCSR." -out ".$pathToCRT." -days ".$duration." -sha256");
-					$check = str_replace("\\", "/", $pathToCRT);
-					if(file_exists($check)) {
-						//Zertifikat Erstellung erfolgreich -> Pfad in DB aktualisieren
-						$update_crt_path = $db->update_request_path_cer($where, $pathToCRT);
-						if(isset($update_crt_path['affected_rows'])){
-							//Alles OK
-							return true;
-						}
-						else {
-							throw new Exception("Aktualisierung des Pfads in der Datenbank fehlgeschlagen!");
-						}
-					}
-					else {
-						throw new Exception("Zertifikatserstellung mit OpenSSL fehlgeschlagen!".$pathToCRT);
-					}
-				}
-				else {
-					throw new Exception("Aktualisierung des Status in der Datenbank fehlgeschlagen!");
-				}
-					
-			}
+require_once('./db.php');
 
+#This function saves a given .csr file to the filesystem 
+#and writes its content to a database
+#Input: $filename name of the uploaded file. 
+
+function putCSR($fileObject, $laufzeit){
+	#Ordner erstellen
+	#Auf einem neuen System muss die uploaddir angepasst werden
+	$uploaddir = 'c:\apache24\ca\kunden\csr\\';
+	#shell_exec("mkdir ".$pathToCSR);
+
+	$uploadfile = $uploaddir . basename($fileObject['name']);
+	//Eindeutigen Namen vergeben
+	$path_parts = pathinfo($uploadfile);
+	$i = 0;
+	while(file_exists($uploadfile)) {
+		//Solange versuchen bis Datei noch nicht exisitert
+		$uploadfile = $path_parts['dirname'] . "\\" . $path_parts['filename'] . "_" . $i . "." . $path_parts['extension'];
+		$i++;
+	}
+	#echo($uploadfile);
+	#exit();
+
+	#php-Datei handler
+	#move_uploaded_file speichert die empfangene Datei
+	#Der Inhalt des Uploads muss noch geprüft werden! 
+	#Mögliche File-Upload Attack
+	#echo '<pre>';
+	if (move_uploaded_file($fileObject['tmp_name'], $uploadfile)) {
+		#echo "File is valid, and was successfully uploaded.\n";
+	} else {
+		// Exception bei Fehlern, werden von Seite ausgegeben
+		throw new Exception("Possible file upload attack!");
+	}
+
+	#echo 'Here is some more debugging info:';
+	#print_r($fileObject);
+
+	#print "</pre>";
+
+
+
+
+
+	#printCSR to website
+	$var = shell_exec('c:\apache24\bin\openssl.exe req -noout -text -in '.$uploadfile);
+	#echo('<pre>'.$var.'</pre>');
+
+	#save to variables
+	$country = substr($var, strpos($var, "C=") + 2, strpos($var, "ST=") - strpos($var, "C=") - 4);
+	$state = substr($var, strpos($var, "ST=") + 3, strpos($var, "L=") - strpos($var, "ST=") - 5);
+	$location = substr($var, strpos($var, "L=") + 2, strpos($var, "O=") - strpos($var, "L=") - 4);
+	$org = substr($var, strpos($var, "O=") + 2, strpos($var, "OU=") - strpos($var, "O=") - 4);
+	$orgunit = substr($var, strpos($var, "OU=") + 3, strpos($var, "CN=") - strpos($var, "OU=") - 5);
+	$domain = substr($var, strpos($var, "CN=") + 3, strpos($var, "Subject Public Key") - strpos($var, "CN=") - 3);
+	$temp = explode("/emailAddress=", $domain);
+	$email = temp[0];
+	unlink($temp);
+
+	#echo("<pre>");
+	#echo('<h3>Country</h3>'.$country);
+	#echo('<h3>State</h3>'.$state);
+	#echo('<h3>Location</h3>'.$location);
+	#echo('<h3>Org</h3>'.$org);
+	#echo('<h3>OrgUnit</h3>'.$orgunit);
+	#echo('<h3>Domain</h3>'.$domain);
+	#echo("</pre>");
+
+	#extracting SANs from the csr file
+	$sanString = strpos($var, "X509v3 Subject Alternative Name");
+
+	if ($sanString === false){
+			#echo("SAN Error");
 		}
+		else {
+			$SANs = explode("DNS", $var);
+
+			for($i = 0; $i < count($SANs); $i++){
+			$SANs[$i] = substr($SANs[$i], 1, -2);
+			#echo($SANs[$i]);
+			#echo("<br>");
+			}
+		}
+		unset($value);
+		#die Sonderfälle Pos 0 und Max abfangen
+		unset($SANs[0]);
+		$temp = explode(" ",$SANs[count($SANs)]);
+		$SANs[count($SANs)] = $temp[0];
+		unlink($temp);
+		#print_r($SANs);
 	
-	#openssl ca -config /home/arne/ssl/certificat.cnf -in /home/arne/ssl/example.com/example.csr -out /home/arne/ssl/example.crt
-	#openssl x509 -req -in c:/apache24/ca/www-server.csr -CA c:/apache24/ca/ica-pub.pem -CAkey c:/apache24/ca/ica-key.pem -CAcreateserial -out c:/apache24/ca/kunden/server-pub-test.crt -days 1095 -sha512
+	#writeToDB
+	$db = new DBAccess();
+	$dbresult = $db->insert_request(date("Y-m-d H:i:s"), date('Y-m-d H:i:s',strtotime(date("Y-m-d H:i:s", time()) . " + ".(365*$laufzeit)." day")), $country, $state, $location, $org, $domain, "1", $orgunit, $email, NULL, NULL, NULL, NULL,$uploadfile, NULL);
 	
+	//Request ID aus DB Rückgabe holen
+	$req_id = $dbresult['id'];
+	
+	//TODO SANS zu Request in DB schreiben
+	
+	#echo '<pre><h3>DB Result</h3>';
+	#print_r($dbresult);
+	#echo '</pre>';
+	
+	//Wenn hier zuvor keine Exception war: ERFOLG -> Request ID zurück geben
+	return $req_id;
+}
+
 ?>
