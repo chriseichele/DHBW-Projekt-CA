357416670c09915fb0a14bb22f87e9f0227b6c36
chriseichele <youshallnotspam+github@gmail.com>
2015-03-06 17:12:42 +0100

    PHP Mailer verwenden, um TLS beim Mailversand zu vereinfachen

--- admin/MailHelper.php
+++ admin/MailHelper.php
@@ -2,17 +2,33 @@
 
 function send_cert_notification_mail($email, $csr_id) {
 	$link = 'wwi12-05.dhbw-heidenheim.de/frontend/viewCSR.php?csr='.$csr_id;
-	
-	$empfaenger = $email;
-	$betreff = 'Projekt CA || Zertifikat genehmigt';
-	$nachricht = 'Ihr Zertifikat wurde genehmigt. Sie k&ouml;nnen es unter folgendem Link abrufen: <br/><a href="'.$link.'">'.$link.'</a>';
-	$header = 'From: noreply@wwi12-05.dhbw-heidenheim.de' . "\r\n" .
-			  'Content-Type: text/html' . "\r\n" .
-    		  'X-Mailer: PHP/' . phpversion();
 
-	$send = mail($empfaenger, $betreff, $nachricht, $header);
+	require_once('./PHP-Mailer/PHPMailerAutoload.php');
 	
-	if(!$send) {
+	$mail = new PHPMailer;
+
+	//$mail->SMTPDebug = 3;                                    // Enable verbose debug output
+
+	$mail->isSMTP();                                           // Set mailer to use SMTP
+	$mail->Host = 'wwi12-05.dhbw-heidenheim.de';  		       // Specify main and backup SMTP servers
+	$mail->SMTPAuth = true;                                    // Enable SMTP authentication
+	$mail->Username = 'no-reply@wwi12-05.dhbw-heidenheim.de';  // SMTP username
+	$mail->Password = 'Himmel12';                              // SMTP password
+	$mail->SMTPSecure = 'tls';                                 // Enable TLS encryption, `ssl` also accepted
+	$mail->Port = 587;                                         // TCP port to connect to
+
+	$mail->From = 'no-reply@wwi12-05.dhbw-heidenheim.de';
+	$mail->FromName = 'Projekt CA';
+	$mail->addAddress($email);
+
+	$mail->isHTML(true);                                       // Set email format to HTML
+
+	$mail->Subject = 'Projekt CA || Account Verifizieren';
+	$mail->Body    = 'Ihr Zertifikat wurde genehmigt. Sie k&ouml;nnen es unter folgendem Link abrufen: <br/><a href="'.$link.'">'.$link.'</a>';
+	$mail->AltBody = 'Ihr Zertifikat wurde genehmigt. Sie können es unter folgendem Link abrufen: '.$link;
+
+	if(!$mail->send()) {
+		//throw new Exception('Mailer Error: ' . $mail->ErrorInfo);
 		throw new Exception('Beim Versenden der Benachrichtigungsmail an den Kunden ist ein unerwarteter Fehler aufgetreten!');
 		return false;
 	} else {
--- frontend/MailHelper.php
+++ frontend/MailHelper.php
@@ -2,18 +2,34 @@
 
 function send_activision_mail($email, $code) {
 	$activision_link = 'wwi12-05.dhbw-heidenheim.de/frontend/activate.php?email='.$email.'&code='.$code;
-	
-	$empfaenger = $email;
-	$betreff = 'Projekt CA || Account Aktivierung';
-	$nachricht = 'Bitte klicken sie folgenden Link an, um ihren Account zu aktivieren: <br/><a href="'.$activision_link.'">'.$activision_link.'</a>';
-	$header = 'From: noreply@wwi12-05.dhbw-heidenheim.de' . "\r\n" .
-			  'Content-Type: text/html' . "\r\n" .
-    		  'X-Mailer: PHP/' . phpversion();
 
-	$send = mail($empfaenger, $betreff, $nachricht, $header);
+	require_once('./PHP-Mailer/PHPMailerAutoload.php');
 	
-	if(!$send) {
-		throw new Exception('Beim Versenden der Aktivierungsmail ist ein unerwarteter Fehler aufgetreten! Bitte kontaktieren Sie uns.');
+	$mail = new PHPMailer;
+
+	//$mail->SMTPDebug = 3;                                    // Enable verbose debug output
+
+	$mail->isSMTP();                                           // Set mailer to use SMTP
+	$mail->Host = 'wwi12-05.dhbw-heidenheim.de';  		       // Specify main and backup SMTP servers
+	$mail->SMTPAuth = true;                                    // Enable SMTP authentication
+	$mail->Username = 'no-reply@wwi12-05.dhbw-heidenheim.de';  // SMTP username
+	$mail->Password = 'Himmel12';                              // SMTP password
+	$mail->SMTPSecure = 'tls';                                 // Enable TLS encryption, `ssl` also accepted
+	$mail->Port = 587;                                         // TCP port to connect to
+
+	$mail->From = 'no-reply@wwi12-05.dhbw-heidenheim.de';
+	$mail->FromName = 'Projekt CA';
+	$mail->addAddress($email);
+
+	$mail->isHTML(true);                                       // Set email format to HTML
+
+	$mail->Subject = 'Projekt CA || Account Verifizieren';
+	$mail->Body    = 'Bitte klicken sie folgenden Link an, um ihren Account zu aktivieren: <br/><a href="'.$activision_link.'">'.$activision_link.'</a>';
+	$mail->AltBody = 'Bitte Kopieren sie folgenden Link in ihre Browserzeile um ihren Account zu aktivieren: $activision_link';
+
+	if(!$mail->send()) {
+		//throw new Exception('Mailer Error: ' . $mail->ErrorInfo);
+		throw new Exception('Beim senden der Account Aktivierungsmail ist ein unerwarteter Fehler aufgetreten. Bitte kontaktieren Sie uns!');
 		return false;
 	} else {
 		return true;
