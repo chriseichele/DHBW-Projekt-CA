5b0291e195c924ba08d3f9c3d3d3ed32bd49ecbd
chriseichele <youshallnotspam+github@gmail.com>
2015-03-10 17:35:44 +0100

    Prüfung ob Log Verzeichnis existiert

--- frontend/LogHelper.php
+++ frontend/LogHelper.php
@@ -2,9 +2,15 @@
 
 class MailLog {
 	
-	private $file = "c:\apache24\logs\frontend_maillog.log";
+	private $dir = "c:\apache24\logs\\";
+	private $file = $path . "frontend_maillog.log";
 	
 	public function add($entry) {
+		if (!file_exists($dir) && !is_dir($dir)) {
+			//Log Verzeichnis erstellen wenn es noch nicht existiert
+    		mkdir($dir);         
+		}
+		//Logeintrag unten dran setzen, in neue Zeile und mit Timestamp
 		file_put_contents($file, date("Y-m-d H:i:s") . " # " . $entry . PHP_EOL, FILE_APPEND);
 	}
 	
