2e61e8e64d0335721d2b1346fb38d3e97fd4f123
NotBuildingSoftware <catch-a-ride@web.de>
2015-03-17 20:09:55 +0100

    Update putCSR.php
    added comments

--- frontend/putCSR.php
+++ frontend/putCSR.php
@@ -1,16 +1,17 @@
 <?php
 require_once('./db.php');
 require_once('./Logger.php');
+#db.php database class
+#Logger.php class is used to Log any openssl related actions
 
 #This function saves a given .csr file to the filesystem 
 #and writes its content to a database
 #Input: $filename name of the uploaded file. 
+#Input: $laufzeit Laufzeit des Zertifikates (1,3 oder 5 Jahre)
+#Input: $intermediate: Ist das angeforderte Zertifikat intermediate (0,1) 
 
 function putCSR($fileObject, $laufzeit, $intermediate){
-	#Ordner erstellen
-	#Auf einem neuen System muss die uploaddir angepasst werden
 	$uploaddir = 'c:\apache24\ca\kunden\csr\\';
-	#shell_exec("mkdir ".$pathToCSR);
 
 	$uploadfile = $uploaddir . basename($fileObject['name']);
 	//Eindeutigen Namen vergeben
@@ -21,28 +22,23 @@ function putCSR($fileObject, $laufzeit, $intermediate){
 		$uploadfile = $path_parts['dirname'] . "\\" . $path_parts['filename'] . "_" . $i . "." . $path_parts['extension'];
 		$i++;
 	}
-	#echo($uploadfile);
-	#exit();
 
 	#php-Datei handler
 	#move_uploaded_file speichert die empfangene Datei
-	#Der Inhalt des Uploads muss noch geprüft werden! 
-	#Mögliche File-Upload Attack
-	#echo '<pre>';
+	#in der validateCSR.php werden diverse Dinge geprüft
 	if (move_uploaded_file($fileObject['tmp_name'], $uploadfile)) {
 		#echo "File is valid, and was successfully uploaded.\n";
 	} else {
 		// Exception bei Fehlern, werden von Seite ausgegeben
-		throw new Exception("Possible file upload attack!");
+		throw new Exception("Error 1 uploading csr");
 	}
 
-	#printCSR to website
+	#open request and save it to a variable
 	$opensslcmd = "c:\apache24\bin\openssl.exe req -noout -text -in ".$uploadfile;
 	$var = shell_exec($opensslcmd);
 	logOS($opensslcmd);
-	#echo('<pre>'.$var.'</pre>');
 
-	#save to variables
+	#split the string and save to variables
 	$country = substr($var, strpos($var, "C=") + 2, strpos($var, "ST=") - strpos($var, "C=") - 4);
 	$state = substr($var, strpos($var, "ST=") + 3, strpos($var, "L=") - strpos($var, "ST=") - 5);
 	$location = substr($var, strpos($var, "L=") + 2, strpos($var, "O=") - strpos($var, "L=") - 4);
@@ -53,9 +49,6 @@ function putCSR($fileObject, $laufzeit, $intermediate){
 	$domain = $temp[0];
 	$email = $temp[1];
 	unset($temp);
-	
-	
-
 
 	#extracting SANs from the csr file
 	$sanString = strpos($var, "X509v3 Subject Alternative Name");
@@ -64,12 +57,11 @@ function putCSR($fileObject, $laufzeit, $intermediate){
 			#if no SANs were found, skip this paragraph
 		}
 		else {
+			#save SANs to Array
 			$SANs = explode("DNS", $var);
 
 			for($i = 0; $i < count($SANs); $i++){
 			$SANs[$i] = substr($SANs[$i], 1, -2);
-			#echo($SANs[$i]);
-			#echo("<br>");
 			}
 		}
 		unset($value);
@@ -82,6 +74,7 @@ function putCSR($fileObject, $laufzeit, $intermediate){
 		$SANs = array_values(array_filter($SANs));
 	
 	#writeToDB
+	#create an entry in the request table
 	$db = new DBAccess();
 	$dbresult = $db->insert_request(date("Y-m-d H:i:s"), date('Y-m-d H:i:s',strtotime(date("Y-m-d H:i:s", time()) . " + ".(365*$laufzeit)." day")), $country, $state, $location, $org, $domain, "1", $orgunit, $email, NULL, NULL, $intermediate, NULL,$uploadfile, NULL);
 	logOS("Ergebnis : ".$dbresult);
@@ -89,17 +82,18 @@ function putCSR($fileObject, $laufzeit, $intermediate){
 	//Request ID aus DB Rückgabe holen
 	$req_id = $dbresult['id'];
 	
-	//Wenn hier zuvor keine Exception war: ERFOLG -> Request ID zurück geben
-	
+	#inserts SANs into san table
 	for($i = 0; $i < count($SANs); $i++){
 		$db->insert_sans($req_id, $SANs[$i]);
 	}
 	if($req_id == "0"){
+		#if db queue failed, return Exception
 		throw new Exception("Die Aktualisierung der DB war nicht erfolgreich!");
 		#del csr from file system
 		unlink($uploadfile);
 	}
 	else{
+	//Wenn hier zuvor keine Exception war: ERFOLG -> Request ID zurück geben	
 	return $req_id;
 	}
 }
