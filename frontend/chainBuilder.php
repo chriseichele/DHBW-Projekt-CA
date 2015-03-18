af28d7c69d266a213aa77847150fe3e89c90d8c3
NotBuildingSoftware <catch-a-ride@web.de>
2015-03-19 00:47:15 +0100

    Update chainBuilder.php

--- frontend/chainBuilder.php
+++ frontend/chainBuilder.php
@@ -20,7 +20,7 @@ $cn = $csr->common_name;
     elseif($type === 'ngnix'){
       $path = "c:\apache24\ca\kunden\\".date("Y-m-d-H-i-s")."_".$cn.".chained.crt";
       file_put_contents($path, file_get_contents($pathToCRT).PHP_EOL);
-      file_put_contents($pathToCRT, file_get_contents("c:\Apache24\ca\ica.crt").PHP_EOL , FILE_APPEND);
+      file_put_contents($pathToCRT, file_get_contents("c:\Apache24\ca\ica.crt") , FILE_APPEND);
       file_put_contents($pathToCRT, file_get_contents("c:\Apache24\ca\ca.crt").PHP_EOL , FILE_APPEND);
       return $path;
     }
@@ -41,7 +41,7 @@ $cn = $csr->common_name;
         elseif($type === 'PEM-Format'){
           $path = "c:\apache24\ca\kunden\\".date("Y-m-d-H-i-s")."_".$cn.".pem";
           file_put_contents($path, file_get_contents($pathToCRT).PHP_EOL);
-          file_put_contents($pathToCRT, file_get_contents("c:\Apache24\ca\ica.crt").PHP_EOL , FILE_APPEND);
+          file_put_contents($pathToCRT, file_get_contents("c:\Apache24\ca\ica.crt"), FILE_APPEND);
           file_put_contents($pathToCRT, file_get_contents("c:\Apache24\ca\ca.crt").PHP_EOL , FILE_APPEND);
           return $path;
         }
