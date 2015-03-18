94e65dc06751d8ff7b4daa3b55a6d5184e6287a5
chriseichele <youshallnotspam+github@gmail.com>
2015-03-19 00:46:36 +0100

    UPDATE

--- frontend/zert.php
+++ frontend/zert.php
@@ -45,8 +45,8 @@ if(isset($_GET['wildcard'])) {
 		<div class="form-group" id="add_sans">
 			<style>#add_sans input, #add_sans button {display: block;min-width:200px;padding:3px;}</style>
 			<label>Weitere SANs hinzuf&uuml;gen:</label>
-			<input type="text" name="sans[san0]" placeholder="weiterer SAN" />
-			<input type="text" name="sans[san1]" placeholder="weiterer SAN" />
+			<input type="text" name="sans[0]" placeholder="SAN 1" />
+			<input type="text" name="sans[1]" placeholder="SAN 2" />
 			<button type="button" id="add_san_line" class="btn btn-default btn-sm" onclick="addSANline();false;">Zeile hinzuf&uuml;gen</button>
 		</div> 
 		<br />
@@ -63,6 +63,6 @@ if(isset($_GET['wildcard'])) {
 
 <script>
 function addSANline() {
-	$("#add_sans button").before('<input type="text" name="sans[san' + $("#add_sans input").length + ']" placeholder="weiterer SAN" />');
+	$("#add_sans button").before('<input type="text" name="sans[' + $("#add_sans input").length + ']" placeholder="SAN '+($("#add_sans input").length+1)+'" />');
 }
 </script>
