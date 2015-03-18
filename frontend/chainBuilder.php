f179459c86b85e22929436266f01edc7ed5249de
NotBuildingSoftware <catch-a-ride@web.de>
2015-03-19 00:16:01 +0100

    Update chainBuilder.php
    Added .pem File

--- frontend/chainBuilder.php
+++ frontend/chainBuilder.php
@@ -18,7 +18,7 @@ $cn = $csr->common_name;
     return $path;
   }
     elseif($type === 'ngnix'){
-      $path = "c:\apache24\ca\kunden\\".date("Y-m-d-H-i-s")."_".$cn.".crt";
+      $path = "c:\apache24\ca\kunden\\".date("Y-m-d-H-i-s")."_".$cn.".chained.crt";
       file_put_contents($path, file_get_contents($pathToCRT).PHP_EOL);
       file_put_contents($pathToCRT, file_get_contents("c:\Apache24\ca\ica.crt").PHP_EOL , FILE_APPEND);
       file_put_contents($pathToCRT, file_get_contents("c:\Apache24\ca\ca.crt").PHP_EOL , FILE_APPEND);
@@ -38,9 +38,16 @@ $cn = $csr->common_name;
         
         return $path;
       }
-        else{
-          throw new Exception("Der gewünschte Zertifikatstyp konnte nicht bereitgestellt werden.");
+        elseif($type === 'PEM-Format'){
+          $path = "c:\apache24\ca\kunden\\".date("Y-m-d-H-i-s")."_".$cn.".pem";
+          file_put_contents($path, file_get_contents($pathToCRT).PHP_EOL);
+          file_put_contents($pathToCRT, file_get_contents("c:\Apache24\ca\ica.crt").PHP_EOL , FILE_APPEND);
+          file_put_contents($pathToCRT, file_get_contents("c:\Apache24\ca\ca.crt").PHP_EOL , FILE_APPEND);
+          return $path;
         }
+          else{
+            throw new Exception("Der gewünschte Zertifikatstyp konnte nicht bereitgestellt werden.");
+          }
 }
 
 ?>
