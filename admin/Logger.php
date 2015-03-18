5fec60055ce0e5775e9048a7063c7879c94014fe
NotBuildingSoftware <catch-a-ride@web.de>
2015-03-18 11:19:11 +0100

    Update Logger.php

--- admin/Logger.php
+++ admin/Logger.php
@@ -1,7 +1,7 @@
 <?php
 
 	function logOS($string){
-		file_put_contents("c:\apache24\ca\openssl.log", $string.PHP_EOL, APPEND_FILE);
+		file_put_contents("c:\apache24\ca\openssl.log", $string.PHP_EOL, FILE_APPEND);
 	}
 
 ?>
