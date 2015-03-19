fae5c527eccfd4d5d66917bc3854fce1743e2f24
NotBuildingSoftware <catch-a-ride@web.de>
2015-03-19 23:07:26 +0100

    Update generateCRT.php
    Bugfix

--- admin/generateCRT.php
+++ admin/generateCRT.php
@@ -8,7 +8,7 @@ require_once('./UserHelper.php');
 		
 		function createCertificate($id){
 			doAdminRightsCheck();
-			$log = new OpensslLogger();
+			$log = new OpensslLogger();6
 
 			#TODO: Abfragen ob der User eingeloggt  ist
 			#get various informations from database used for signing the certificate
@@ -37,7 +37,7 @@ require_once('./UserHelper.php');
 					if($checkSAN != NULL){
 						#create certificate with SANs
 						getSANs($id, 0);
-						$opensslcmd = "c:\apache24\bin\openssl.exe x509 -req -CA c:\apache24\ca\ica.crt -CAkey c:\apache24\ca\ica.key -CAcreateserial -in ".$pathToCSR." -out ".$pathToCRT." -days ".$duration." -sha256 -extensions v3_req -extfile c:\apache24\htdocs\dev\arne\openssl.cnf";
+						$opensslcmd = "c:\apache24\bin\openssl.exe x509 -req -CA c:\apache24\ca\ica.crt -CAkey c:\apache24\ca\ica.key -CAcreateserial -in ".$pathToCSR." -out ".$pathToCRT." -days ".$duration." -sha256 -extensions v3_req -extfile c:\apache24\ca\kunden\openssl.cnf";
 						#sign certificate
 						shell_exec($opensslcmd);
 						#write Command and config to log
@@ -90,13 +90,13 @@ require_once('./UserHelper.php');
 		subjectAltName = @alt_names
 		[ alt_names ]".PHP_EOL;
 
-		file_put_contents("c:\apache24\htdocs\dev\arne\openssl.cnf", $configContent);
+		file_put_contents("c:\apache24\ca\kunden\openssl.cnf", $configContent);
 		#lookup SANs in DB and attach them to the config file
 		$db = new DBAccess();
 		$where = array("request_id","=","'".$id."'");
 		$db_result = $db->get_sans_all_where($where);
 		for($i = 0; $i < count($db_result); $i++){
-			file_put_contents("c:\apache24\htdocs\dev\arne\openssl.cnf", "DNS.".$i." = ".$db_result[$i]->name.PHP_EOL, FILE_APPEND);
+			file_put_contents("c:\apache24\ca\kunden\openssl.cnf", "DNS.".$i." = ".$db_result[$i]->name.PHP_EOL, FILE_APPEND);
 		}
 
 	}
