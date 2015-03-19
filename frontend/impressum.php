223aa6c682708f2be4a72e97066428aba432594e
Chris <chriseichele@users.noreply.github.com>
2015-03-19 14:23:30 +0100

    Update impressum.php

--- frontend/impressum.php
+++ frontend/impressum.php
@@ -9,7 +9,7 @@ include('./header.php');
     <div class="container">
       <h2>Impressum</h2>
       <p>Dies ist ein Studentenprojekt an der DHBW-Heidenheim im Kurs Wirtschaftsinformatik 2012</p>
-      <p>Der Code ist OpenSource und <a href="https://github.com/chriseichele/DHBW-Projekt-CA">hier in GitHub</a> zu finden.</p>
+      <p>Der Code ist OpenSource und <a target="_blank" href="https://github.com/chriseichele/DHBW-Projekt-CA">hier in GitHub</a> zu finden.</p>
     </div>
       
 <?php include('./footer.php'); ?>
