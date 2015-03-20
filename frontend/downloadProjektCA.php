ac2f3af7aa2f2e7b2ac9aa9b4b977ae169476a64
chriseichele <youshallnotspam+github@gmail.com>
2015-03-20 09:55:48 +0100

    Download können auch direkt aus dem Pub Verzeichnis genommen werden

--- frontend/downloadProjektCA.php
+++ frontend/downloadProjektCA.php
@@ -1,51 +1,21 @@
 <?php
 
-if(isset($_GET['download'])) {
+$pagetitle = 'Projekt CA Zertifikate';
 
-	$download = $_GET['download'];
+include('./header.php');
 
-	if($download == "ca") {
-		$path = "c:\apache24\ca\ca.crt";
-	}
-	elseif($download == "ica") {
-		$path = "c:\apache24\ca\ica.crt";
-	}
-	else {
-		$path = NULL;
-	}
-	if(!empty($path)) {
-		//Passenden Datentyp im HTTP Header setzen
-		header ( "Content-Type: application/octet-stream" );
-		//Passenden Dateinamen im Download-Requester vorgeben
-		$save_as_name = basename ( $path );
-		header ( "Content-Disposition: attachment; filename=\"$save_as_name\"" );
-		// Datei ausgeben.
-		readfile ( $path );
-	} else {
-  		$_SESSION['message']['warning'][] = "Ung&uuml;ltiger Download!";
-  		header('Location: '.$_SERVER['PHP_SELF']);
-  		exit();
-	}
-
-} else {
-
-	$pagetitle = 'Projekt CA Zertifikate';
-
-	include('./header.php');
-	
-	echo' <div class="jumbotron">
-		<div class="container">
-			<h1>'. $pagetitle. '</h1>
-			<p>Hier k&ouml;nnen sie unsere Zertifikate herunterladen.</p>
-		</div>
-	</div>
+echo' <div class="jumbotron">
 	<div class="container">
-		<a href="'.$_SERVER['PHP_SELF'].'?download=ca" class="btn btn-success btn-lg btn-block" role="button">Root Zertifikat</a>
-		<a href="'.$_SERVER['PHP_SELF'].'?download=ica" class="btn btn-success btn-lg btn-block" role="button">Intermediate Zertifikat</a>
-	</div>';
+		<h1>'. $pagetitle. '</h1>
+		<p>Hier k&ouml;nnen sie unsere Zertifikate herunterladen.</p>
+	</div>
+</div>
+<div class="container">
+	<a href="./pub/ca.crt" class="btn btn-success btn-lg btn-block" role="button">Root Zertifikat</a>
+	<a href="./pub/ica.crt" class="btn btn-success btn-lg btn-block" role="button">Intermediate Zertifikat</a>
+</div>';
 
-	include('./footer.php'); 
+include('./footer.php'); 
 
-}
 
 ?>
